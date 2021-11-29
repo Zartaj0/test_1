4536a6b75b368d24b94a606079daa56919abaa15
Zartaj0 <91191500+Zartaj0@users.noreply.github.com>
2021-11-29 19:00:07 +0530

    Initial Commit

---
+++ interval.js
@@ -0,0 +1,18 @@
+var t=0
+
+setInterval( ()=>{
+    if (t<2000) {
+        t += 4
+    console.log(`${t}seconds is passed`)
+    
+        }
+        function ifEven(t){
+    if(t%2===0){
+        console.log("number is even")
+    }else{
+        console.log("number is odd")
+    }
+}
+    ifEven(t)
+
+    },4000)
